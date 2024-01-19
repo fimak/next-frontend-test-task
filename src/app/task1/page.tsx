55d66f44885cefb9835b7c1915bb86b545ce4e06
Aleksandr Ufimtsev <fimak@bk.ru>
2024-01-19 21:51:01 +0700

    task1

--- src/app/task1/page.tsx
+++ src/app/task1/page.tsx
@@ -1,3 +1,22 @@
+const moveZeros = (option: any[]): any[] => {
+  const basedArr = option.filter((el: any) => el !== 0);
+  const length = option.length - basedArr.length;
+  return [...basedArr, Array(length).fill(0)];
+}
+
 export default function Page() {
-  return <h1>Hello, task1</h1>
+  const option = [false,1, 0, 1, 2, 0, 1, 3, 'a'];
+  const result = moveZeros(option);
+
+  return (
+    <div className="w-full h-[100vh] flex items-center justify-center">
+      <div>
+        <h1>Task1</h1>
+        <div>
+          <p>Array: {option.toString()}</p>
+          <p>Result: {result.toString()}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
